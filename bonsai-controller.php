04b3a40c4059affc7f95e22f7ef1ff9406dab17f
mrwindupbirdesq <nmaahs11@gmail.com>
2024-05-08 09:20:45 -0700

    Create update method

--- bonsai-controller.php
+++ bonsai-controller.php
@@ -18,7 +18,6 @@ class BonsaiController
         $photo_url = $_POST['photo_url'];
 
         try {
-            // Prepare SQL statement
             $stmt = $this->db->prepare("INSERT INTO bonsais (species, origin_story, geolocation, photo_url) 
             VALUES (:species, :origin_story, :geolocation, :photo_url)");
             $stmt->bindParam(':species', $species);
@@ -55,6 +54,48 @@ class BonsaiController
             $stmt->execute();
             $stmt->setFetchMode(PDO::FETCH_ASSOC);
             $result = $stmt->fetchAll();
+            return $result;
+        } catch (PDOException $e) {
+            return $e->getMessage();
+        }
+    }
+    public function updateBonsai($id)
+    {
+        $fieldsToUpdate = [];
+        $data = [];
+
+        if (isset($_POST['species'])) {
+            $fieldsToUpdate[] = "species = :species";
+            $data['species'] = Utilities::sanitize($_POST['species']);
+        }
+        if (isset($_POST['origin_story'])) {
+            $fieldsToUpdate[] = "origin_story = :origin_story";
+            $data['origin_story'] = Utilities::sanitize($_POST['origin_story']);
+        }
+        if (isset($_POST['geolocation'])) {
+            $fieldsToUpdate[] = "geolocation = :geolocation";
+            $data['geolocation'] = Utilities::sanitize($_POST['geolocation']);
+        }
+        if (isset($_POST['photo_url'])) {
+            $fieldsToUpdate[] = "photo_url = :photo_url";
+            $data['photo_url'] = $_POST['photo_url'];
+        }
+
+        if (empty($fieldsToUpdate)) {
+            http_response_code(400);
+            echo json_encode(["error" => "No data included to update"]);
+        }
+
+        $sql = "UPDATE bonsais SET " . implode(', ', $fieldsToUpdate) . " WHERE id = :id";
+        $data['id'] = $id;
+
+        try {
+            $stmt = $this->db->prepare($sql);
+            foreach ($data as $key => &$value) {
+                $stmt->bindParam(':' . $key, $value);
+            }
+            $result = $stmt->execute();
+
             return $result;
         } catch (PDOException $e) {
             return $e->getMessage();
