60821fd365d4810085297e3f3a00bdcce03a9044
mrwindupbirdesq <nmaahs11@gmail.com>
2024-05-09 16:11:32 -0700

    Create get user method

--- controllers/user-controller.php
+++ controllers/user-controller.php
@@ -35,4 +35,19 @@ class UserController
             echo json_encode($e->getMessage());
         }
     }
+
+    public function getUser($id)
+    {
+        try {
+            $stmt = $this->db->prepare("SELECT * FROM users WHERE id=$id");
+            $stmt->execute();
+            $stmt->setFetchMode(PDO::FETCH_ASSOC);
+            $result = $stmt->fetchAll();
+            http_response_code(200);
+            echo json_encode($result);
+        } catch (PDOException $e) {
+            http_response_code(500);
+            echo json_encode($e->getMessage());
+        }
+    }
 }
