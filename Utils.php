2d8d22b9e2f90c800f8c88b931828b8afad7bbd9
mrwindupbirdesq <nmaahs11@gmail.com>
2024-05-13 11:40:08 -0700

    Create dummy user validation function For development purposes only. This is a placeholder function.

--- Utils.php
+++ Utils.php
@@ -24,4 +24,11 @@ class Utilities
         }
         return $input;
     }
+    // Minimal implementation for development purposes
+    public static function validateUser($providedCredentials, $requestedCredentials) {
+        if ($providedCredentials === $requestedCredentials) return true;
+        else {
+            return false;
+        }
+    }
 }
